ab4c4e724941f8d5b29cf96f930645959a9563d3
ochrist2 <ochrist2@uwo.ca>
2017-01-12 21:44:49 -0500

    updated

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { ScotchyScotchPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('scotchy-scotch App', function() {
+  let page: ScotchyScotchPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new ScotchyScotchPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
